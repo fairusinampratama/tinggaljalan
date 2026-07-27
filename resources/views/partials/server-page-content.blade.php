f7716199261c538370d8651799707f437e462a34
Fairus In'am Pratama <fairusinampratama@gmail.com>
2026-07-27 07:32:18 +0700

    Reduce SEO fallback bold tags

--- resources/views/partials/server-page-content.blade.php
+++ resources/views/partials/server-page-content.blade.php
@@ -38,9 +38,9 @@
                                 >
                             @endif
                             @if (! empty($item['url']) && ! empty($item['title']))
-                                <p><a href="{{ $item['url'] }}"><strong>{{ $item['title'] }}</strong></a></p>
+                                <p><a href="{{ $item['url'] }}">{{ $item['title'] }}</a></p>
                             @elseif (! empty($item['title']))
-                                <p><strong>{{ $item['title'] }}</strong></p>
+                                <p>{{ $item['title'] }}</p>
                             @endif
                             @if (! empty($item['text']))
                                 <p>{{ $item['text'] }}</p>
